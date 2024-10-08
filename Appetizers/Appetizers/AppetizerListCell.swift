c36dd938a1d7186d5556f57725f8327ccee7e363
miktaba <mtabakaev@gmail.com>
2024-10-08 14:15:52 +0200

    Work with ListView
    - Created and added ListCell

---
+++ Appetizers/Appetizers/AppetizerListCell.swift
@@ -0,0 +1,38 @@
+//
+//  AppetizerCellView.swift
+//  Appetizers
+//
+//  Created by Mikhail Tabakaev on 10/8/24.
+//
+
+import SwiftUI
+
+struct AppetizerListCell: View {
+    
+    let appetizer: Appetizer
+    
+    var body: some View {
+        HStack {
+            Image("asian-flank-steak")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 90)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                
+                Text("$\(appetizer.price, specifier: "%.2f")")
+                    .foregroundStyle(.secondary)
+                    .fontWeight(.semibold)
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+#Preview {
+    AppetizerListCell(appetizer: MockData.sampleAppetizer)
+}
--- Appetizers/Appetizers/AppetizerListView.swift
+++ Appetizers/Appetizers/AppetizerListView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView{
-            Text("Appetizer List View")
-                .navigationTitle("🍟 Appetizers")
+            List(MockData.appetisers) { appetizer in
+                AppetizerListCell(appetizer: appetizer)
+            }
+            .navigationTitle("🍟 Appetizers")
         }
     }
 }
