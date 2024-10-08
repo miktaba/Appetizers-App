0e83ee4f8cca994c0ff1c8315aa37b92864a2010
miktaba <mtabakaev@gmail.com>
2024-10-08 14:37:32 +0200

    Added NetworkManager

---
+++ Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Mikhail Tabakaev on 10/8/24.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
---
+++ Appetizers/Appetizers/NetworkManager.swift
@@ -0,0 +1,52 @@
+//
+//  NetworkManager.swift
+//  Appetizers
+//
+//  Created by Mikhail Tabakaev on 10/8/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    private let appetizerURL = baseURL + "appetizers"
+    
+    private init() {}
+    
+    
+    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+            guard let _ = error else {
+                completed(.failure(.unableToComplete))
+                return
+                
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+                completed(.success(decodedResponse.request))
+            } catch {
+                completed(.failure(.invalidURL))
+            }
+        }
+        task.resume()
+    }
+}
